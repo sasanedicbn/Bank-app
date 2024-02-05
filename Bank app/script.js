602f7bd85cdab3a8bf0c0b245c069398028d4e46
Sasa Nedic <svetasmak0@gmail.com>
2024-02-05 21:08:41 +0100

    Finish project

--- Bank app/script.js
+++ Bank app/script.js
@@ -1,21 +1,15 @@
 import accounts from "./data";
-const labelWelcome = document.querySelector(".welcome");
-const labelDate = document.querySelector(".date");
+
 const labelBalance = document.querySelector(".balance__value");
 const labelSumIn = document.querySelector(".summary__value--in");
 const labelSumOut = document.querySelector(".summary__value--out");
-const labelSumInterest = document.querySelector(".summary__value--interest");
-
 const containerApp = document.querySelector(".app");
-const containerMovements = document.querySelector(".movements");
-
 const btnLogin = document.querySelector(".login__btn");
 const btnTransfer = document.querySelector(".form__btn--transfer");
 const btnLoan = document.querySelector(".form__btn--loan");
 const btnClose = document.querySelector(".form__btn--close");
 const btnSort = document.querySelector(".btn--sort");
 const btnDeleteAcc = document.querySelector(".close-account");
-
 const inputLoginUsername = document.querySelector(".login__input--user");
 const inputLoginPin = document.querySelector(".login__input--pin");
 const inputTransferTo = document.querySelector(".form__input--to");
